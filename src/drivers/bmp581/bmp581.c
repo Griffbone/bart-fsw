69090ac98ff42896e494a0248c1ad5df63c5b86c
Griffin Desktop <gjourda@gmail.com>
2025-03-22 04:28:39 -0400

    add bmp581 pressure and temperature read functionality

--- src/drivers/bmp581/bmp581.c
+++ src/drivers/bmp581/bmp581.c
@@ -1,7 +1,85 @@
 #include "bmp581.h"
 #include "stm32h7xx_hal.h"
 
-enum bmp581_err bmp581_init(struct bmp581_device *device);
+enum bmp581_err bmp581_init(struct bmp581_device *device) {
+    // TODO: error handling
+
+    // Recommended power-up procedure from page 18 of the datasheet
+    uint8_t buf;
+
+    // Wait t_powerup
+    bmp581_delay(3);
+
+    // Check chip ID is not zero
+    bmp581_read_byte(device, BMP581_REG_CHIP_ID, buf);
+
+    if (buf != 0) {
+        return BMP581_ERR_GENERAL;
+    }
+    
+    // Check status register
+    bmp581_read_byte(device, BMP581_REG_CHIP_STATUS, buf);
+    uint8_t status_nvm_ready = (buf >> 1) && 0x01;
+    uint8_t status_nvm_err = (buf >> 2) && 0x01;
+    
+    if (status_nvm_ready == 0 || status_nvm_err == 1) {
+        return BMP581_ERR_GENERAL;
+    }
+
+    // Read interrupt status register
+    bmp581_read_byte(device, BMP581_REG_INT_STATUS, buf);
+    uint8_t int_status_por = (buf >> 4) && 0x01;
+
+    if (int_status_por != 1) {
+        return BMP581_ERR_GENERAL;
+    }
+
+    // Enter standby mode 
+    bmp581_write_byte(device, BMP581_REG_ODR_CONFIG, BMP581_PWR_STANDBY);
+    bmp581_delay(3);
+
+    // Set measurement configuration
+    // Enable pressure measurment
+    bmp581_write_byte(device, BMP581_REG_OSR_CONFIG, 1 << 6);
+    // TODO: SET ODR CONFIGURATION HERE
+
+    // Enter normal mode 
+    bmp581_write_byte(device, BMP581_REG_ODR_CONFIG, BMP581_PWR_NORMAL);
+
+    return BMP581_ERR_OK;    
+}
+
+enum bmp581_err bmp581_soft_reset(struct bmp581_device *device) {
+    // TODO: add error handling
+    bmp581_write_byte(device, BMP581_REG_CMD, BMP581_COMMAND_SOFT_REST);
+    bmp581_delay(2);
+
+    return BMP581_ERR_OK;
+}
+
+enum bmp581_err bmp581_rad_temp(struct bmp581_device *device, float temp) {
+    uint8_t buf[3]; 
+    int32_t temp_raw; 
+
+    bmp581_read_block(device, BMP581_REG_TEMP_XLSB, 3, buf);
+
+    temp_raw = ((buf[2] << 16) | (buf[1] << 8) | buf[0]);
+    temp = temp_raw/65536.0f;
+    
+    return BMP581_ERR_OK;
+}
+
+enum bmp581_err bmp581_read_press(struct bmp581_device *device, float press) {
+    uint8_t buf[3];
+    int32_t press_raw;
+
+    bmp581_read_block(device, BMP581_REG_PRESS_XLSB, 3, buf);
+
+    press_raw = ((buf[2] << 16) | (buf[1] << 8) | buf[0]);
+    press = press_raw/64.0f;
+
+    return BMP581_ERR_OK;
+}
 
 enum bmp581_err bmp581_write_byte(struct bmp581_device *device, uint8_t reg, uint8_t data) {
     uint8_t ret;
@@ -35,6 +113,24 @@ enum bmp581_err bmp581_read_byte(struct bmp581_device *device, uint8_t reg, uint
     }
 
     *data = rx;
+
+    return BMP581_ERR_OK;
+}
+
+enum bmp581_err bmp581_read_block(struct bmp581_device *device, uint8_t start_reg, uint8_t bytes, uint8_t *data) {
+    uint8_t ret;
+
+    ret = HAL_I2C_Mem_Read(device->hi2c, device->i2c_addr << 1, start_reg, 1, data, bytes, HAL_MAX_DELAY);
+
+    if (ret != HAL_OK) {
+        return BMP581_ERR_HAL;
+    }
+
+    return BMP581_ERR_OK;
+}
+
+enum bmp581_err bmp581_delay(uint32_t ms) {
+    HAL_Delay(delay);
 
     return BMP581_ERR_OK;
 }
--- src/drivers/bmp581/bmp581.h
+++ src/drivers/bmp581/bmp581.h
@@ -9,9 +9,9 @@
 #include <stdint.h>
 
 /* LIS3MDL Register Map */
-#define BMP581_REG_ASIC_ID          0x01
-#define BMP581_REG_ASIC_REV_ID      0x02
-#define BMP581_REG_ASIC_STATUS      0x11
+#define BMP581_REG_CHIP_ID          0x01
+#define BMP581_REG_REV_ID           0x02
+#define BMP581_REG_CHIP_STATUS      0x11
 
 #define BMP581_REG_TEMP_XLSB        0x1D
 #define BMP581_REG_TEMP_LSB         0x1E
@@ -21,8 +21,23 @@
 #define BMP581_REG_PRESS_LSB        0x21
 #define BMP581_REG_PRESS_MSB        0x22
 
+#define BMP581_REG_INT_STATUS       0x27
+
 #define BMP581_REG_STATUS           0x28
 #define BMP581_REG_CMD              0x7E
+
+#define BMP581_REG_ODR_CONFIG       0x37
+#define BMP581_REG_OSR_CONFIG       0x36
+#define BMP581_REG_OOR_CONFIG       0x35
+
+/* REG_ODR_CONFIG configuration Map */
+#define BMP581_PWR_STANDBY          0x00
+#define BMP581_PWR_NORMAL           0x01
+#define BMP581_PWR_FORCED           0x02
+#define BMP581_PWR_NONSTOP          0x03
+
+/* BMP581 Command Map */
+#define BMP581_COMMAND_SOFT_REST    0xB6
 
 /* Device ID */
 #define BMP581_ADDR_SDO_HIGH    0x47
@@ -31,6 +46,7 @@
 enum bmp581_err {
     BMP581_ERR_OK,
     BMP581_ERR_HAL,
+    BMP581_ERR_GENERAL,
 };
 
 struct bmp581_device {
@@ -40,7 +56,15 @@ struct bmp581_device {
 };
 
 enum bmp581_err bmp581_init(struct bmp581_device *device);
+enum bmp581_err bmp581_soft_reset(struct bmp581_device *device);
+
+enum bmp581_err bmp581_rad_temp(struct bmp581_device *device, float temp);
+enum bmp581_err bmp581_read_press(struct bmp581_device *device, float press);
+
 enum bmp581_err bmp581_write_byte(struct bmp581_device *device, uint8_t reg, uint8_t data);
 enum bmp581_err bmp581_read_byte(struct bmp581_device *device, uint8_t reg, uint8_t *data);
+enum bmp581_err bmp581_read_block(struct bmp581_device *device, uint8_t start_reg, uint8_t bytes, uint8_t *data);
+
+enum bmp581_err bmp581_delay(uint32_t ms);
 
 #endif /* __BMP581_H__*/
